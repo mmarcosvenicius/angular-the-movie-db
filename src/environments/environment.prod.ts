8f8f376f49334f6d865868c5b8e78231abc6b4cb
Marcos Venicius <41882652+mmarcosvenicius@users.noreply.github.com>
2022-10-28 15:02:05 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   urlApi: 'https://api.themoviedb.org/3',
   apiKey: '4fbe5f1580ce04baa051cd5f38f06bc1',
-  apiUrl: 'https://my-json-server.typicode.com/mmarcosvenicius/passaro-urbano-bd'
+  apiUrl: 'https://my-json-server.typicode.com/mmarcosvenicius/bd'
 };
